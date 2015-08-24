ff4ef1b4e765aa9fda38298719b21fdfb45d7046
xdizzy12 <mathieu.1995@live.nl>
2015-08-24 11:28:18 +0200

    Added notes

--- index.php
+++ index.php
@@ -4,4 +4,21 @@
  * User: Mathieu
  * Date: 24-8-2015
  * Time: 11:22
+ *
+ * Inloggen
+Registreren
+Pagina’s aanpassen
+Strakke opmaak
+Maximaal 3 kleuren
+________________________________________
+--DATABASE--
+Users
+User_name
+User_pass
+Pages
+Page1_title
+Page2_title
+Page1_content
+Page2_content
+
  */
